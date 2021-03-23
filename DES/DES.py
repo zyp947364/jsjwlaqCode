a46730bface44c24c8afab47d1e95e7212fa7ef0
zyp <zyp947364@163.com>
2021-03-23 21:33:01 +0800

    加上分组加密，可以加密任意位数的明文。注：密钥是64位的。

--- DES/DES.py
+++ DES/DES.py
@@ -5,47 +5,109 @@ from F_fanc import F_fanc
 from inverse_IP import IV_IP
 
 
+ZeroNum = 0 # 记录零的个数
+Key = [] # 记录每组的子密钥
+
+class DES():
+    
+    @staticmethod
+    def desEncode(Plaintext,keyseed):
+        init_ip = init_IP.init_IP(Plaintext)
+        L = init_ip[0:32]
+        R = init_ip[32:64]
+        global Key
+        for i in range(1,17):
+            key = GetKeys.GetKeys(keyseed,i)
+            K,keyseed = key.split(",")
+            Key.append(K)
+            L_ = R
+            after_F = F_fanc.F_fanc(R,K)
+            F_int = int(after_F,2)
+            L_int = int(L,2)
+            R_ = str(bin(F_int^L_int).split("0b")[1])
+            while R_.__len__()<32:
+                R_ = "0"+R_
+            L = L_
+            R = R_
+        msg = R+L
+        Ciphertext_ascii = IV_IP.IV_IP(msg)
+        Ciphertext = Tools.AsciiToString(Ciphertext_ascii)
+        return Ciphertext
+
+
+    @staticmethod
+    def desDecode(Ciphertext):
+        init_ip = init_IP.init_IP(Ciphertext)
+        L = init_ip[0:32]
+        R = init_ip[32:64]
+        global Key
+        for i in range(1,17):
+            K = Key[16-i]
+            L_ = R
+            after_F = F_fanc.F_fanc(R,K)
+            F_int = int(after_F,2)
+            L_int = int(L,2)
+            R_ = str(bin(F_int^L_int).split("0b")[1])
+            while R_.__len__()<32:
+                R_ = "0"+R_
+            L = L_
+            R = R_
+        msg = R+L
+        Plaintext_ascii = IV_IP.IV_IP(msg)
+        return Plaintext_ascii
+
+
+    @staticmethod
+    def Group_desEncode(Plaintext,keyseed):
+        Plaintext_ascii = Tools.stringToAscii(Plaintext)
+        GroupNum = len(Plaintext_ascii) // 64 # 判断有几组明文
+        global ZeroNum
+        if len(Plaintext_ascii)/64 > GroupNum:
+            #若是分不均匀则在加一组
+            GroupNum += 1
+
+        Plaintext_G = []
+        for i in range(0,GroupNum):
+            # 明文分组
+            Plaintext_G.append(Plaintext_ascii[64*i:64*(i+1)])
+
+        if len(Plaintext_G[GroupNum-1]) < 64:
+            #最后一组不够64，补零
+            while len(Plaintext_G[GroupNum-1]) < 64:
+                Plaintext_G[GroupNum-1] += "0"
+                ZeroNum += 1
+        Ciphertext = ""
+        for i in Plaintext_G:
+            Ciphertext_G = DES.desEncode(i,keyseed)
+            Ciphertext += Ciphertext_G
+        return Ciphertext
+    
+
+    @staticmethod
+    def Group_desDecode(Ciphertext):
+        Ciphertext_ascii = Tools.stringToAscii(Ciphertext)
+        global ZeroNum
+        GroupNum = len(Ciphertext_ascii) // 64
+        Ciphertext_G = []
+        for i in range(0,GroupNum):
+            Ciphertext_G.append(Ciphertext_ascii[64*i:64*(i+1)])
+        Plaintext_ascii = ""
+        for i in Ciphertext_G:
+            Plaintext_G = DES.desDecode(i)
+            Plaintext_ascii += Plaintext_G
+        Plaintext_ascii = Plaintext_ascii[0:(len(Plaintext_ascii)-ZeroNum)]
+        Plaintext = Tools.AsciiToString(Plaintext_ascii)
+        
+        return Plaintext
+
+
+
+
 if __name__ == '__main__':
-    msg = input("请输入8明文:")
-    keyseed = input("请输入8密钥")
-    msg_ascii = Tools.stringToAscii(msg)
-    init_ip = init_IP.init_IP(msg_ascii)
-    L = init_ip[0:32]
-    R = init_ip[32:64]
-    Key = []
-    for i in range(1,17):
-        key = GetKeys.GetKeys(keyseed,i)
-        K,keyseed = key.split(",")
-        Key.append(K)
-        L_ = R
-        after_F = F_fanc.F_fanc(R,K)
-        F_int = int(after_F,2)
-        L_int = int(L,2)
-        R_ = str(bin(F_int^L_int).split("0b")[1])
-        while R_.__len__()<32:
-            R_ = "0"+R_
-        L = L_
-        R = R_
-    msg = R+L
-    Ciphertext_ascii = IV_IP.IV_IP(msg)
-    Ciphertext = Tools.AsciiToString(Ciphertext_ascii)
-    print("密文为:"+Ciphertext)
-    init_ip = init_IP.init_IP(Ciphertext_ascii)
-    L = init_ip[0:32]
-    R = init_ip[32:64]
-    for i in range(1,17):
-        K = Key[16-i]
-        L_ = R
-        after_F = F_fanc.F_fanc(R,K)
-        F_int = int(after_F,2)
-        L_int = int(L,2)
-        R_ = str(bin(F_int^L_int).split("0b")[1])
-        while R_.__len__()<32:
-            R_ = "0"+R_
-        L = L_
-        R = R_
-    msg = R+L
-    Plaintext_ascii = IV_IP.IV_IP(msg)
-    Plaintext = Tools.AsciiToString(Plaintext_ascii)
+    Plaintext = input("请输入明文:")
+    keyseed = input("请输入密钥种子(默认12345678)") or "12345678"
+    Ciphertext = DES.Group_desEncode(Plaintext,keyseed)
+    print("加密后密文为:"+Ciphertext)
+    Plaintext = DES.Group_desDecode(Ciphertext)
     print("解密后明文为:"+Plaintext)
 
