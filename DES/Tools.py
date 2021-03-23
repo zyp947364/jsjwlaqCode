58dfaa642055e068e33cc706fefd44e08aa7a912
zyp <zyp947364@163.com>
2021-03-23 17:00:56 +0800

    第一部分初始置换写完

---
+++ DES/Tools.py
@@ -0,0 +1,24 @@
+# 一些方法函数
+class Tools:
+    @staticmethod
+    def complete8bits(string):
+	# "补全为8位二进制数"
+	    while string.__len__()<8:
+		    string="0"+string
+	    return string
+
+
+    @staticmethod
+    def stringToAscii(string):
+	# "返回字符串用二进制ascii码的表示值"
+	    Rst=""
+	    for x in range(0,string.__len__()):
+		    Rst+=Tools.complete8bits(bin(ord(string[x])).split("0b")[1])
+	    return Rst
+
+    @staticmethod
+    def AsciiToString(m):
+	    Message=""
+	    for x in range(0,m.__len__()//8):
+		    Message+=chr(int(m[x*8:(x+1)*8],2))
+	    return Message
---
+++ DES/init_IP.py
@@ -0,0 +1,33 @@
+# 初始置换代码
+from Tools import Tools
+
+class init_IP:
+    def __init__(self,Plaintext):
+        self.Plaintext = Plaintext
+        self.fin = ""
+        self.Ip_table = [ 
+    58,50,42,34,26,18,10,2,
+	60,52,44,36,28,20,12,4,
+	62,54,46,38,30,22,14,6,
+	64,56,48,40,32,24,16,8,
+	57,49,41,33,25,17,9,1,
+	59,51,43,35,27,19,11,3,
+	61,53,45,37,29,21,13,5,
+	63,55,47,39,31,23,15,7 ]
+    def init_IP(self):
+        
+        if len(self.Plaintext)!=64:
+            print("输入信息有误，errors in init_IP")
+            return        
+        else:
+            for i in range(0,64):
+                self.fin+=self.Plaintext[self.Ip_table[i]-1]
+		#print("=======IP初始置换结束=======")
+        print("输入：",Tools.AsciiToString(self.Plaintext),"\n结果：",Tools.AsciiToString(self.fin))
+        return self.fin
+if __name__ == '__main__':
+	## 这里只能测试8位的
+	Plaintext = input("输入明文")
+	P_ascii = Tools.stringToAscii(Plaintext)
+	init_ip = init_IP(P_ascii)
+	init_ip.init_IP()
